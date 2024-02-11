cead04ada91deda9067b88e90947588be0cf787f
D <113527210+Kardarigan@users.noreply.github.com>
2024-02-11 12:40:59 -0700

    Update whatsDinner.py

--- whatsDinner.py
+++ whatsDinner.py
@@ -1,7 +1,7 @@
 import random
 
-lunchMeals = ["Qormesabzi", "Tahchin Morgh", "Kima", "Dampokhtak", "Adas Polo", "Sabzi Polo with Morgh", "Zereshk Polo with Morgh (Sospaz)", "Loobia Polo", "Kookoo Sibzamini", "Kalam Polo", "Gooshtekoofteh", "Nokhod Polo", "Joojeh Tabe'e", "Chelo Kabab"]
-dinnerMeals = ["Kookoo Sibzamini","Kookoo Sabzi","Pasta","Kashkebademjoon","Kotlet Sibzaminikham","Shirazian Kotlet", "Kotlet with Sibzamini Pokhte", "Sandewich from birron","Ash","Loobia Pokhte","Lasagna","Pirashky","Samosas","Egg", "Adassi", "Nugett", "Tonemahi","Pizza","Noodle","Salad Macarony","Olivier salad", "Falafel","Donair","Abgosht","Fileh sookhary with frenchrise","Dopiazeh", "Chelo Kabab"]
+lunchMeals = ["Qormesabzi", "Tahchin Morgh", "Kima", "Dampokhtak", "Adas Polo", "Sabzi Polo with Morgh", "Zereshk Polo with Morgh (Sospaz)", "Loobia Polo", "Kookoo Sibzamini", "Kalam Polo", "Gooshtekoofteh", "Nokhod Polo", "Joojeh Tabe'e", "Chelo Kabab","Egg"]
+dinnerMeals = ["Kookoo Sibzamini","Kookoo Sabzi","Pasta","Kashkebademjoon","Kotlet Sibzaminikham","Shirazian Kotlet", "Kotlet with Sibzamini Pokhte", "Sandewich from birron","Ash","Loobia Pokhte","Lasagna","Pirashky","Samosas","Egg", "Adassi", "Nugett", "Tonemahi","Pizza","Noodle","Salad Macarony","Olivier salad", "Falafel","Donair","Abgosht","Fileh sookhary with frenchrise","Dopiazeh", "Chelo Kabab","Burger"]
 
 while True:
     theMeal = input("What kind of meal do you have in mind to offer?[l/d] ")
