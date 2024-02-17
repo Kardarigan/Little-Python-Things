fa9699343846c77a11d3b743e7518b34b3b10d70
D <113527210+Kardarigan@users.noreply.github.com>
2024-02-17 01:06:44 -0700

    Update whatsDinner.py

--- whatsDinner.py
+++ whatsDinner.py
@@ -3,28 +3,33 @@ import random
 lunchMeals = ["Qormesabzi", "Tahchin Morgh", "Kima", "Dampokhtak", "Adas Polo", "Sabzi Polo with Morgh", "Zereshk Polo with Morgh (Sospaz)", "Loobia Polo", "Kookoo Sibzamini", "Kalam Polo", "Gooshtekoofteh", "Nokhod Polo", "Joojeh Tabe'e", "Chelo Kabab","Egg"]
 dinnerMeals = ["Kookoo Sibzamini","Kookoo Sabzi","Pasta","Kashkebademjoon","Kotlet Sibzaminikham","Shirazian Kotlet", "Kotlet with Sibzamini Pokhte", "Sandewich from birron","Ash","Loobia Pokhte","Lasagna","Pirashky","Samosas","Egg", "Adassi", "Nugett", "Tonemahi","Pizza","Noodle","Salad Macarony","Olivier salad", "Falafel","Donair","Abgosht","Fileh sookhary with frenchrise","Dopiazeh", "Chelo Kabab","Burger"]
 
-suggestedMeals = set()
+suggestedLunches = set()
+suggestedDinner = set()
 
 while True:
     theMeal = input("What kind of meal do you have in mind to offer?[l/d] ")
+
     if theMeal == "l":
-        if len(suggestedMeals) == len(lunchMeals) + len(dinnerMeals):
+        if len(suggestedLunches) == len(lunchMeals):
+            print("I Suggested all Meals that i knew ;)")
+            pass
+        else:
             meal = random.choice(lunchMeals)
-            while meal in suggestedMeals:
+            while meal in suggestedLunches:
                 meal = random.choice(lunchMeals)
-        else:
-            print("I Suggested all Meals that i knew ;)")
-        suggestedMeals.add(meal)
-        print(f"I suggest you {meal} :)")
+            suggestedLunches.add(meal)
+            print(f"I suggest you {meal} :)")
+
     elif theMeal == "d":
-        if len(suggestedMeals) == len(dinnerMeals) + len(dinnerMeals):
-            meal = random.choice(dinnerMeals)
-            while meal in suggestedMeals:
-                meal = random.choice(dinnerMeals)
-        else:
+        if len(suggestedDinner) == len(dinnerMeals):
             print("I Suggested all Meals that i knew ;)")
-        suggestedMeals.add(meal)
-        print(f"I suggest you {meal} :)")
+            pass
+        else:
+            meal = random.choice(dinnerMeals)
+            while meal in suggestedDinner:
+                meal = random.choice(dinnerMeals)
+            suggestedDinner.add(meal)
+            print(f"I suggest you {meal} :)")
     else:
         print("Try again [l/d]")
         pass
